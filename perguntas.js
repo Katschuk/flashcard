09d0afeb0f8382fe2ebc5fa5b3325b9089215426
Katschuk <katschuk.rocha@escola.pr.gov.br>
2025-08-14 08:31:14 -0300

    ....

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Mecânica',
+    'Qual e o nome da peça que faz a resfriação do motor?',
+    'Radiador'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Cenário Automotivo',
+    'Qual foi o ano do primeiro salão do automovel?',
+    'O primeiro Salão do Automóvel no Brasil foi realizado em 1960. O evento ocorreu no Pavilhão de Exposições do Parque Ibirapuera, em São Paulo'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Volkswagen',
+    'Em qual ano foi criado o motor Ap?',
+    'O motor AP foi criado em 1985 pela Volkswagen do Brasil, com o nome "Alta Performance"'
 )
     criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Volkswagen',
+    'Quando saiu o primeiro Volkswagen Santana?',
+    'O Santana foi lançado no Brasil em 1984, como o primeiro sedã médio de luxo da Volkswagen no país. Ele chegou para competir com o Chevrolet Monza e o Ford Del Rey, e foi baseado na segunda geração do Passat europeu.'
 )
